cbbe013f0dfc791c262ff989e5355dfe1b488590
Sosane Mahamoud Houssein <sosanemh@proton.me>
2026-04-08 11:38:36 -0400

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.go
@@ -0,0 +1,12 @@
+func hasDuplicate(nums []int) bool {
+    m := make(map[int]bool)
+
+    for _, n := range(nums) {
+        if _, ok := m[n]; ok {
+            return true
+        }
+        m[n] = true
+    }
+
+    return false
+}
